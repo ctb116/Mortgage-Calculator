9901597e1bb91e3ccc5c8eef792303fb2b2f8a3e
Catherine Bradley <catherinetybradley@gmail.com>
2019-08-16 12:17:09 -0700

    add mortgage system input

--- src/com/ctb116/Main.java
+++ src/com/ctb116/Main.java
@@ -1,9 +1,22 @@
 package com.ctb116;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("Hello");
+        System.out.println("Calculate your Mortgage");
+
+        Scanner scanner = new Scanner(System.in);
+        //get principal
+        System.out.print("Principal: ");
+        int principal = scanner.nextInt();
+        //get annual interest
+        System.out.print("Annual Interest: ");
+        float annualInterest = scanner.nextFloat();
+        //get years
+        System.out.print("Period (Years): ");
+        byte period = scanner.nextByte();
+        
     }
 }
